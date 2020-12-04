1495a5f9731f079b9cae59fc5453d5c7216cacb8
Saravanavasan <vasan@Ramadasss-MacBook-Air.local>
2020-12-04 10:21:46 -0800

    Added basic script for running the modules

--- runner.py
+++ runner.py
@@ -2,11 +2,17 @@ from maftm import A1Z26, Base64, RSA
 cipherType = input("What cipher would you like to do? Type 1 for RSA, 2 for AZ126, and 3 for BASE64. ")
 if cipherType == "1":
     n = int(input("Gimme dat modulus variable: "))
-    int(n)
     d, e = RSA.GetKeys(n)
     ciphertext = input("Gimme Ciphertext: ")
     Decrypted_text = RSA.Decrypt(d, n, ciphertext)
     print(Decrypted_text)
     
 
-
+elif cipherType == "2":
+    cipher = input("Gimme cipher: ")
+    decrypted  = A1Z26.Decrypt(cipher)
+    print(decrypted)
+elif cipherType == "3":
+    cipher = input("Gimme cipher: ")
+    decrypted  = Base64.Decrypt(cipher)
+    print(decrypted)
